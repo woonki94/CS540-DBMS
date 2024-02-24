04a6f7f887d0e2e51cdfdd76ba6e1c9647c81ce3
Kim Woonki <woonki94@naver.com>
2024-02-23 20:52:41 -0800

    put record to buffer and sort.

--- assignment4/main.cpp
+++ assignment4/main.cpp
@@ -1,31 +1,117 @@
 /* This is a skeleton code for External Memory Sorting, you can make modifications as long as you meet 
-   all question requirements*/  
+   all question requirements*/
 
-#include <bits/stdc++.h>
+#include <string>
+#include <ios>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <cmath>
+#include <algorithm>
 #include "record_class.h"
 
 using namespace std;
 
-//defines how many blocks are available in the Main Memory 
+//defines how many blocks are available in the Main Memory
 #define buffer_size 22
+#define BLOCK_SIZE 1536
 
 Records buffers[buffer_size]; //use this class object of size 22 as your main memory
 
-/***You can change return type and arguments as you want.***/
+void FillBufferFromFile(Records buffers[buffer_size], fstream &dataFile){
 
+    // Grab 22 employ records from the file
+    for(int i=0; i<22; i++){
+        // Get one record
+        Records empRecord = Grab_Emp_Record(dataFile);
+
+        // No more records in the file
+        if(empRecord.no_values == -1){
+            break;
+        }
+
+        // Insert each field data into emp_record
+        buffers[i].emp_record.age = empRecord.emp_record.age;
+        buffers[i].emp_record.eid = empRecord.emp_record.eid;
+        buffers[i].emp_record.ename = empRecord.emp_record.ename;
+        buffers[i].emp_record.salary = empRecord.emp_record.salary;
+
+        // For checking the total number of records
+        buffers->number_of_emp_records += 1;
+    }
+
+}
+
+void PrintBufferEmployeeInfo(){
+    for (int j=0; j<buffer_size; j++){
+        cout<<"eid:"<<buffers[j].emp_record.eid<<", ";
+        cout<<"ename:"<<buffers[j].emp_record.ename<<", ";
+        cout<<"age:"<<buffers[j].emp_record.age<<", ";
+        cout<<"salary:"<<buffers[j].emp_record.salary<<endl;
+    }
+}
+
+static bool compareByEmployeeId(const Records& a, const Records& b){
+    return a.emp_record.eid < b.emp_record.eid;
+}
+
+void sortRecordsByEmployeeId(){
+    sort(buffers, buffers+buffer_size, compareByEmployeeId);
+}
+
+string serialize(Records buffers[])
+{
+    ostringstream serializedRecord;
+    serializedRecord.write(reinterpret_cast<const char *>(&buffers->emp_record.eid), sizeof(int));
+    serializedRecord << buffers->emp_record.ename <<',';
+    serializedRecord.write(reinterpret_cast<const char*>(&buffers->emp_record.age), sizeof(int)) << ',';                                                                 // serialize string bio, variable length
+    serializedRecord.write(reinterpret_cast<const char *>(&buffers->emp_record.salary), sizeof(double));
+    return serializedRecord.str();
+}
+
+void writeRecordToRuns(Records buffers[], int startOffset, fstream &runFile){
+    int nextFreeSpace;
+    int numRecords;
+    int recordLength;
+
+    for(int cnt=0; cnt<buffer_size; cnt++){
+        string serializedRecord = serialize(&buffers[cnt]);
+        recordLength = serializedRecord.size();
+
+
+    }
+}
+
+
+
+/***You can change return type and arguments as you want.***/
 //PASS 1
-//Sorting the buffers in main_memory and storing the sorted records into a temporary file (Runs) 
-void Sort_Buffer(){
+//Sorting the buffers in main_memory and storing the sorted records into a temporary file (Runs)
+void Sort_Buffer(Records buffers[buffer_size], fstream &runFile){
     //Remember: You can use only [AT MOST] 22 blocks for sorting the records / tuples and create the runs
+
+    // Sort records in the buffer
+    sortRecordsByEmployeeId();
+
+    // Insert records into the Run page
+
+
+    // Write the Run page to the Run File
+
+
     return;
 }
 
 //PASS 2
-//Use main memory to Merge the Runs 
-//which are already sorted in 'runs' of the relation Emp.csv 
+//Use main memory to Merge the Runs
+//which are already sorted in 'runs' of the relation Emp.csv
 void Merge_Runs(){
+    //and store the Sorted results of your Buffer using PrintSorted()
 
-    //and store the Sorted results of your Buffer using PrintSorted() 
     return;
 }
 
@@ -40,19 +126,47 @@ int main() {
     //Open file streams to read and write
     //Opening out the Emp.csv relation that we want to Sort
     fstream empin;
-    empin.open("Emp.csv", ios::in);
-   
+    empin.open("Emp2.csv", ios::in);
+    if(!empin.is_open()){
+        cerr << "Error opening file: "<<endl;
+        return 1;
+    }
+
     //Creating the EmpSorted.csv file where we will store our sorted results
     fstream SortOut;
     SortOut.open("EmpSorted.csv", ios::out | ios::app);
+    if(!SortOut.is_open()){
+        cerr << "Error opening the file to write"<<endl;
+        return 1;
+    }
 
     //1. Create runs for Emp which are sorted using Sort_Buffer()
+    fstream Runs;
+    Runs.open("run.csv", ios::out | ios::binary);
+    if(!Runs.is_open()){
+        cerr << "Error opening the file to write" << endl;
+        return 1;
+    }
 
+    //1-1. Fill buffer with 22 employee records
+    FillBufferFromFile(buffers, empin);
 
-    //2. Use Merge_Runs() to Sort the runs of Emp relations 
+    //1-2. Sort records in the buffer and write to the Run File
+    Sort_Buffer(buffers, Runs);
+    //PrintBufferEmployeeInfo();
+
+    writeRecordsToRuns(buffers);
+
+    //2. Use Merge_Runs() to Sort the runs of Emp relations
 
 
     //Please delete the temporary files (runs) after you've sorted the Emp.csv
+
+
+
+    empin.close();
+    SortOut.close();
+    Runs.close();
 
     return 0;
 }
--- assignment4/record_class.h
+++ assignment4/record_class.h
@@ -1,15 +1,24 @@
 /* This is a skeleton code for External Memory Sorting, you can make modifications as long as you meet 
-   all question requirements*/  
+   all question requirements*/
 /* This record_class.h contains the class Records, which can be used to store tuples form Emp.csv (stored
 in the EmpRecord).
 */
-#include <bits/stdc++.h>
+#include <string>
+#include <ios>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <cmath>
 
 using namespace std;
 
 class Records{
-    public:
-    
+public:
+
     struct EmpRecord {
         int eid;
         string ename;
@@ -21,12 +30,14 @@ class Records{
 
     int no_values = 0; //You can use this to check if you've don't have any more tuples
     int number_of_emp_records = 0; // Tracks number of emp_records you have on the buffer
+
 };
 
 // Grab a single block from the Emp.csv file and put it inside the EmpRecord structure of the Records Class
 Records Grab_Emp_Record(fstream &empin) {
     string line, word;
     Records emp;
+
     // grab entire line
     if (getline(empin, line, '\n')) {
         // turn line into a stream
@@ -47,3 +58,13 @@ Records Grab_Emp_Record(fstream &empin) {
         return emp;
     }
 }
+
+void initBucket(int startOffset, fstream &dataFile){
+    // Seek to the appropriate position at the end of the current page
+    dataFile.seekp(startOffset - sizeof(int));
+    //int initFreeSpace = 0;
+    int initRecordNumInPage = 0;
+    dataFile.write(reinterpret_cast<const char*>(&initRecordNumInPage), sizeof(int));
+    //dataFile.write(reinterpret_cast<const char*>(&initFreeSpace), sizeof(int));
+}
+
